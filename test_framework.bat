b5a0e194459b74bd8e0efaf8289395a4f7695f66
Igor Anpilogov <id.anpilogov@gmail.com>
2021-08-03 20:27:40 +0400

    ADD: tracking shot

---
+++ test_framework.bat
@@ -0,0 +1,3 @@
+@RD /S /Q "D:\Games\Anomaly\MO2\mods\Community Task Pack"
+XCOPY /e /i /q "D:\Tasks\Task Packs\Community Task Pack" "D:\Games\Anomaly\MO2\mods\Community Task Pack"
+START "" "D:\Desktop\MO2Anomaly.lnk"
